fe8317e41c808b153ab64007c4d3bb39a3685739
Sai Krishna <kanapuramsaikrishna1999@gmail.com>
2026-06-30 20:54:27 +0530

    added event for calculate button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInput } from './user-input/user-input.component';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,27 @@ import { UserInput } from './user-input/user-input.component';
   standalone: true
 })
 export class App {
+
+  onCaluculateInvestmentResults(event: { initialInvestment: number; annualInvestment: number; expectedReturn: number; duration: number }) {
+    const annualData = [];
+    let investmentValue = event.initialInvestment;
+
+    for (let i = 0; i < event.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (event.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + event.annualInvestment;
+      const totalInterest =
+        investmentValue - event.annualInvestment * year - event.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: event.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: event.initialInvestment + event.annualInvestment * year,
+      });
+    }
+    console.log(annualData);
+    return annualData;
+  }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
+  standalone: true
 })
-export class UserInput {}
+export class UserInput {
+  @Output() userInput = new EventEmitter<{
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }>();
+  enteredInitialInvestment= "0";
+  enteredAnnualInvestment= "0";
+  enteredExpectedReturn= "0";
+  enteredDuration= "0";
+  onSubmit() {
+    this.userInput.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration
+    });
+  }
+}
